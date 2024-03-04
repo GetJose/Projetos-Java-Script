95379a6df3b543ad59a75d0265ba2c56b15f46fe
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2024-03-04 07:37:59 -0300

    adicionando o conteudo da pagina de apresentação junto com todas as imagens

--- Projeto 2 - Relógio Digital/script.js
+++ Projeto 2 - Relógio Digital/script.js
@@ -1,16 +1,14 @@
-let currentDate = new Date();
-let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let monthOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-let dayOfMonth = currentDate.getDate();
-let dayOfWeek = daysOfWeek[currentDate.getDay()];
-let month = monthOfYear[currentDate.getMonth()];
-let year = currentDate.getFullYear();
-let day = currentDate.getDay();
-let hour = currentDate.getHours();
-let minute = String(currentDate.getMinutes()).padStart(2, '0');
-
-function updateClock() {
+function updateClock() {let currentDate = new Date();
+    let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let monthOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let dayOfMonth = currentDate.getDate();
+    let dayOfWeek = daysOfWeek[currentDate.getDay()];
+    let month = monthOfYear[currentDate.getMonth()];
+    let year = currentDate.getFullYear();
+    let day = currentDate.getDay();
+    let hour = currentDate.getHours();
+    let minute = String(currentDate.getMinutes()).padStart(2, '0');
     let clockText = `${dayOfWeek} ${dayOfMonth} ${month} ${year}`
     document.querySelector('.date').innerText = clockText;
     let time = `${hour}:${minute}`
--- Projeto 3 - Relógio Analógico/script.js
+++ Projeto 3 - Relógio Analógico/script.js
@@ -23,11 +23,6 @@ function updateClock() {
 }
 
 function fixZero(time) {
-    //if(time<10){
-    //	return '0'+time;
-    //}else{
-    //	return time;
-    //}
     return time < 10 ? `0${time}` : time;
 }
 
